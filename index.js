2eb42407d42ecb17b2c1426e0042c4e7d1c79f96
Ray Zana <rayzana280@gmail.com>
2022-12-18 12:45:12 -0500

    commit tree 07217f5d5f4877178766345e98e3887fed589350

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(scuber){
+    return scuber.slice(0,2);
+}
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const returnLastTwoDrivers = function(scuber1){
+    return scuber1.slice(2,4);
+}
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num1){
+    return function(num2){
+        return num2*num1;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+fareDoubler(4);
+
+const fareTripler = createFareMultiplier(3)
+
+fareTripler(3);
+
+function selectDifferentDrivers(array, returnFirst){
+    return returnFirst(array);
+}
+selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers);
+selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnLastTwoDrivers);
+ 
+
+
